ebd66c785f3625693febcbf78504fb3c7122902b
MigVarona <migvaronag@gmail.com>
2024-04-03 22:39:21 +0200

    pr

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,62 @@ export default function Home() {
   return (
     <div>
       <Cabezera />
+      <nav className="hidden md:block">
+        <div className="flex items-center  justify-center flex-col">
+          <div className="flex items-center  max-w-5xl justify-center">
+           
+              <h1
+                className="font-bold mr-17 text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl m-0 p-0 leading-none"
+                style={{
+                  letterSpacing: "-0.1em",
+                  marginBottom: "0",
+                  marginTop: "0",
+                  color: "red"
+                }}
+              >
+                CSS
+              </h1>
+           
+            <div className="vertical-line">
+              <h1
+                className="font-bold ml-2 text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl m-0 p-0 leading-none"
+                style={{
+                  letterSpacing: "-0.1em",
+                  marginBottom: "0",
+                  marginTop: "0",
+                }}
+              >
+                HOLA
+              </h1>
+            </div>
+            <div className="vertical-line">
+              <h1
+                className="font-bold ml-2 text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl m-0 p-0 leading-none"
+                style={{
+                  letterSpacing: "-0.1em",
+                  marginBottom: "0",
+                  marginTop: "0",
+                }}
+              >
+                GRID
+              </h1>
+            </div>
+            <div className="vertical-line">
+              <h1
+                className="font-bold ml-2 text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl m-0 p-0 leading-none"
+                style={{
+                  letterSpacing: "-0.1em",
+                  marginBottom: "0",
+                  marginTop: "0",
+                }}
+              >
+                HOME
+              </h1>
+            </div>
+          </div>
+          <div className="border-b  border-black w-full max-w-6xl"></div>
+        </div>
+      </nav>
     </div>
   );
 }
