e67afefaa9653e6d082b52d50e67db9feca6c9ff
MigVarona <migvaronag@gmail.com>
2024-04-05 09:51:53 +0200

    Contac react hook form

---
+++ src/app/api/email/route.jsx
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+
+export async function POST(request) {
+  const { email, name, message } = await request.json();
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.MY_EMAIL,
+    to: process.env.MY_EMAIL,
+    subject: `Message from ${name} (${email})`,
+    text: message,
+  };
+
+  const sendMailPromise = () =>
+    new Promise((resolve, reject) => {
+      transport.sendMail(mailOptions, function (err) {
+        if (!err) {
+          resolve('Email sent');
+        } else {
+          reject(err.message);
+        }
+      });
+    });
+
+  try {
+    await sendMailPromise();
+    return new Response(JSON.stringify({ message: 'Email sent' }), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err }), { status: 500 });
+  }
+}
