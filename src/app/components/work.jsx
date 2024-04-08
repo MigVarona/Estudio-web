84391badaed37b813451870e969410b9badd8719
MigVarona <migvaronag@gmail.com>
2024-04-08 12:27:40 +0200

    pr

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -4,12 +4,15 @@ function Work() {
   return ( 
     <section className="flex items-center justify-center">
       <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/2 text-center"> <img src="/work2.png" alt="" /></div>
-        <div className="w-full md:w-1/2 text-center"> <img src="/work2.png" alt="" /></div>
+        <div className="w-full md:w-1/2 text-center"> 
+          <Image src="/work2.png" alt="Work 1" width={600} height={500} />
+        </div>
+        <div className="w-full md:w-1/2 text-center"> 
+          <Image src="/work2.png" alt="Work 2" width={600} height={500} />
+        </div>
       </div>
     </section>
   );
 }
 
 export default Work;
-
